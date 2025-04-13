ef0fe5e3ab0f5d17c4f25720c4b51110d51e9c7f
afham-haleema <afhamhaleema@gmail.com>
2025-04-13 22:14:16 +0300

    Added files

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger=document.querySelector('.navbar .hamburger');
+const navlinks=document.querySelector('.nav-items');
+
+hamburger.addEventListener('click',()=>{
+    if(navlinks.classList.contains('active')){
+        navlinks.classList.remove('active')
+    }
+   else{
+    navlinks.classList.add('active')
+   }
+})
+
+AOS.init();
+
